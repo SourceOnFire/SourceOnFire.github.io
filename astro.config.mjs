f739793fa679e315bb8565a8bcdaf0c77f3a3d9d
J. Cuéllar <73777890+j-c7@users.noreply.github.com>
2026-09-16 00:14:32 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
   trailingSlash: 'never',
   markdown: {
     shikiConfig: {
-      theme: 'everforest-dark',
+      theme: 'gruvbox-dark-hard',
     },
   },
   site: "https://SourceOnFire.github.io",
