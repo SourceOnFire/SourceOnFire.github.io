f659736a8e0ab0d3f4bb1569503f28d2293d93a7
J. Cuéllar <73777890+j-c7@users.noreply.github.com>
2026-09-15 22:33:07 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
   trailingSlash: 'never',
   markdown: {
     shikiConfig: {
-      theme: 'one-dark-pro',
+      theme: 'vesper',
     },
   },
   site: "https://SourceOnFire.github.io",
